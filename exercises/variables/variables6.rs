cce401920ef73e8e149141d1aabb1e332e49624c
focksor <focksor@outlook.com>
2023-01-09 23:00:01 +0800

    solve variables6

--- exercises/variables/variables6.rs
+++ exercises/variables/variables6.rs
@@ -1,9 +1,7 @@
 // variables6.rs
 // 执行 `rustlings hint variables6` 或在观察模式下使用 `hint` 子命令来获取提示。
 
-// I AM NOT DONE
-
-const NUMBER = 3;
+const NUMBER: i32 = 3;
 fn main() {
     println!("数字 {}", NUMBER);
 }
