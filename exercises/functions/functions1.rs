b4e26766ec06c08ef318e36708ed621b0e68b6d9
focksor <focksor@outlook.com>
2023-01-09 23:14:18 +0800

    solve functions1

--- exercises/functions/functions1.rs
+++ exercises/functions/functions1.rs
@@ -1,8 +1,11 @@
 // functions1.rs
 // 执行 `rustlings hint functions1` 或在观察模式下使用 `hint` 子命令来获取提示。
 
-// I AM NOT DONE
-
 fn main() {
     call_me();
+}
+
+fn call_me() {
+    // Rust 并不在乎函数定义的位置，只要定义了就行
+    // 这个函数在main之前定义也是一样的
 }
