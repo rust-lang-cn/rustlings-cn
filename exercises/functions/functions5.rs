500301371b8659bb753ce89b83593110719b976d
focksor <focksor@outlook.com>
2023-01-09 23:48:43 +0800

    solve functions5

--- exercises/functions/functions5.rs
+++ exercises/functions/functions5.rs
@@ -1,13 +1,11 @@
 // functions5.rs
 // 执行 `rustlings hint functions5` 或在观察模式下使用 `hint` 子命令来获取提示。
 
-// I AM NOT DONE
-
 fn main() {
     let answer = square(3);
     println!("3的平方是 {}", answer);
 }
 
 fn square(num: i32) -> i32 {
-    num * num;
+    num * num
 }
