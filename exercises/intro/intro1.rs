b48fdf3ed035bd9950ef42b9fc8769f17ea8a874
focksor <focksor@outlook.com>
2023-01-09 21:43:57 +0800

    chinese intro1.rs

--- exercises/intro/intro1.rs
+++ exercises/intro/intro1.rs
@@ -1,19 +1,19 @@
 // intro1.rs
-// About this `I AM NOT DONE` thing:
-// We sometimes encourage you to keep trying things on a given exercise, even
-// after you already figured it out. If you got everything working and feel
-// ready for the next exercise, remove the `I AM NOT DONE` comment below.
-// Execute `rustlings hint intro1` or use the `hint` watch subcommand for a hint.
+// 关于 `I AM NOT DONE` 这件事：
+// 我们有时候鼓励你一直尝试给定的练习，甚至
+// 当你已经解决了它之后。如果你一切顺利并且感觉
+// 你已经准备好迎接下一个练习，删除下面的注释行 `I AM NOT DONE`。
+// 执行 `rustlings hint intro1` 或者在观察模式下使用 `hint` 子命令以获取提示。
 //
-// If you're running this using `rustlings watch`: The exercise file will be reloaded
-// when you change one of the lines below! Try adding a `println!` line, or try changing
-// what it outputs in your terminal. Try removing a semicolon and see what happens!
+// 如果你正在通过 `rustlings watch` 运行：这个练习文件将会在你改变下面的某一行时被重载！
+// 尝试添加一行 `println!`，或者尝试改变它在你终端中输出的内容。
+// 尝试删除一个分号并看看会发生什么！
 
 // I AM NOT DONE
 
 fn main() {
-    println!("Hello and");
-    println!(r#"       welcome to...                      "#);
+    println!("你好");
+    println!(r#"       欢迎来到...                      "#);
     println!(r#"                 _   _ _                  "#);
     println!(r#"  _ __ _   _ ___| |_| (_)_ __   __ _ ___  "#);
     println!(r#" | '__| | | / __| __| | | '_ \ / _` / __| "#);
@@ -21,10 +21,10 @@ fn main() {
     println!(r#" |_|   \__,_|___/\__|_|_|_| |_|\__, |___/ "#);
     println!(r#"                               |___/      "#);
     println!();
-    println!("This exercise compiles successfully. The remaining exercises contain a compiler");
-    println!("or logic error. The central concept behind Rustlings is to fix these errors and");
-    println!("solve the exercises. Good luck!");
+    println!("这个练习编译成功了。剩余的练习包含了一个编译");
+    println!("或逻辑错误。Rustlings 背后的核心思想是修复这些错误并且");
+    println!("解决练习。祝你好运！");
     println!();
-    println!("The source for this exercise is in `exercises/intro/intro1.rs`. Have a look!");
-    println!("Going forward, the source of the exercises will always be in the success/failure output.");
+    println!("这个练习的源码在 `exercises/intro/intro1.rs` 处。去看一看吧！");
+    println!("今后，练习的源码将总是会输出 成功/失败。");
 }
