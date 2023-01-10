1d672ca5ba3dfd0247fa0dd15c911f8bda803240
focksor <focksor@outlook.com>
2023-01-10 22:22:50 +0800

    solve primitive_types6

--- exercises/primitive_types/primitive_types6.rs
+++ exercises/primitive_types/primitive_types6.rs
@@ -3,13 +3,11 @@
 // 你可以在 ??? 处放置表达式来获取第二个元素以通过测试用例。
 // 执行 `rustlings hint primitive_types6` 或在观察模式下使用 `hint` 子命令来获取提示。
 
-// I AM NOT DONE
-
 #[test]
 fn indexing_tuple() {
     let numbers = (1, 2, 3);
     // 替换下面 ??? 为元组索引表达式。
-    let second = ???;
+    let second = numbers.1;
 
     assert_eq!(2, second,
         "这不是元组的第2个元素！")
