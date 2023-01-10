06b262ac4f4be31fd5dd055dc22aa55bd085850c
focksor <focksor@outlook.com>
2023-01-10 21:24:38 +0800

    solve primitive_types1

--- exercises/primitive_types/primitive_types1.rs
+++ exercises/primitive_types/primitive_types1.rs
@@ -2,8 +2,6 @@
 // 填写代码行的缺失部分
 // 没有提示，没有技巧，只要习惯键入这些 :)
 
-// I AM NOT DONE
-
 fn main() {
     // Booleans (`bool`)
 
@@ -12,7 +10,7 @@ fn main() {
         println!("Good morning!");
     }
 
-    let // 像示例一样完成这行！或者将其设为 false ！
+    let is_evening = false; // 像示例一样完成这行！或者将其设为 false ！
     if is_evening {
         println!("Good evening!");
     }
