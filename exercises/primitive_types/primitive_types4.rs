c6a8772379a2cf4d1f331e14553a5ff22d31a066
focksor <focksor@outlook.com>
2023-01-10 22:01:34 +0800

    solve primitive_types4

--- exercises/primitive_types/primitive_types4.rs
+++ exercises/primitive_types/primitive_types4.rs
@@ -2,13 +2,11 @@
 // 在 ??? 处获取一个数组的切片来使测试可以通过。
 // 执行 `rustlings hint primitive_types4` 或在观察模式下使用 `hint` 子命令来获取提示。
 
-// I AM NOT DONE
-
 #[test]
 fn slice_out_of_array() {
     let a = [1, 2, 3, 4, 5];
 
-    let nice_slice = ???
+    let nice_slice = &a[1..=3];
 
     assert_eq!([2, 3, 4], nice_slice)
 }
