b9734d22d2ecb6c11d6d49b7b7da1c6e13dc9ee2
focksor <focksor@outlook.com>
2023-01-10 20:22:07 +0800

    chinese if1

--- exercises/if/if1.rs
+++ exercises/if/if1.rs
@@ -4,10 +4,10 @@
 // I AM NOT DONE
 
 pub fn bigger(a: i32, b: i32) -> i32 {
-    // Complete this function to return the bigger number!
-    // Do not use:
-    // - another function call
-    // - additional variables
+    // 完成这个函数使它返回一个较大值
+    // 不要使用：
+    // - 其它函数调用
+    // - 额外的变量
 }
 
 // Don't mind this for now :)
