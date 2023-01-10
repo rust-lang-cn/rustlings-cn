708e6af58639c7fd584659ab2f77538ce40ea6e8
focksor <focksor@outlook.com>
2023-01-10 22:41:12 +0800

    chinese vecs1

--- exercises/vecs/vecs1.rs
+++ exercises/vecs/vecs1.rs
@@ -1,4 +1,6 @@
 // vecs1.rs
+// 你的任务是创建一个 `Vec`，它要与数组 `a` 的元素相同。
+// 让我可以编译并通过测试！
 // Your task is to create a `Vec` which holds the exact same elements
 // as in the array `a`.
 // Make me compile and pass the test!
@@ -7,8 +9,8 @@
 // I AM NOT DONE
 
 fn array_and_vec() -> ([i32; 4], Vec<i32>) {
-    let a = [10, 20, 30, 40]; // a plain array
-    let v = // TODO: declare your vector here with the macro for vectors
+    let a = [10, 20, 30, 40]; // 一个平平无奇的数组
+    let v = // TODO: 使用 vectors 宏在这里定义你的 vector
 
     (a, v)
 }
