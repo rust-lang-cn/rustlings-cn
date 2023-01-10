f44615da3bb8072cc2663784363304651b24abfd
focksor <focksor@outlook.com>
2023-01-10 22:12:36 +0800

    solve primitive_types5

--- exercises/primitive_types/primitive_types5.rs
+++ exercises/primitive_types/primitive_types5.rs
@@ -3,11 +3,9 @@
 // Destructure the `cat` tuple so that the println will work.
 // 执行 `rustlings hint primitive_types5` 或在观察模式下使用 `hint` 子命令来获取提示。
 
-// I AM NOT DONE
-
 fn main() {
     let cat = ("Furry McFurson", 3.5);
-    let /* 在这里写你的模式 */ = cat;
+    let (name, age) = cat;
 
     println!("{} 已经 {} 岁了。", name, age);
 }
