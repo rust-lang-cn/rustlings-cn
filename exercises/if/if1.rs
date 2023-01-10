8f38bc788761820ba1a7c2f7b8c1c481b6036dc3
focksor <focksor@outlook.com>
2023-01-10 20:45:24 +0800

    solve if1

--- exercises/if/if1.rs
+++ exercises/if/if1.rs
@@ -1,13 +1,25 @@
 // if1.rs
 // 执行 `rustlings hint if1` 或在观察模式下使用 `hint` 子命令来获取提示。
 
-// I AM NOT DONE
-
 pub fn bigger(a: i32, b: i32) -> i32 {
     // 完成这个函数使它返回一个较大值
     // 不要使用：
     // - 其它函数调用
     // - 额外的变量
+
+    // 由于if是表达式，所以在if之前加上return也是可以的
+    // 例如：
+    // return if a > b {
+    //     a
+    // } else {
+    //     b
+    // }
+
+    if a > b {
+        a
+    } else {
+        b
+    }
 }
 
 // Don't mind this for now :)
