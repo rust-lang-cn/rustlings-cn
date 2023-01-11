6cd92fdb0a976548cb8c5fa8a00a41a2eef70a5a
focksor <focksor@outlook.com>
2023-01-11 22:08:03 +0800

    solve move_semantics6

--- exercises/move_semantics/move_semantics6.rs
+++ exercises/move_semantics/move_semantics6.rs
@@ -2,24 +2,22 @@
 // 执行 `rustlings hint move_semantics6` 或在观察模式下使用 `hint` 子命令来获取提示。
 // 除了添加或删除引用之外你不能改变任何东西。
 
-// I AM NOT DONE
-
 fn main() {
     let data = "Rust is great!".to_string();
 
-    get_char(data);
+    get_char(&data);
 
-    string_uppercase(&data);
+    string_uppercase(data);
 }
 
 // 应该不获取所有权
-fn get_char(data: String) -> char {
+fn get_char(data: &String) -> char {
     data.chars().last().unwrap()
 }
 
 // 应该获取所有权
-fn string_uppercase(mut data: &String) {
-    data = &data.to_uppercase();
+fn string_uppercase(mut data: String) {
+    data = data.to_uppercase();
 
     println!("{}", data);
 }
