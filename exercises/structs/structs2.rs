3a304523725a451f9b9e6a1ff42ea82da0768e50
focksor <focksor@outlook.com>
2023-01-11 22:35:35 +0800

    solve structs2

--- exercises/structs/structs2.rs
+++ exercises/structs/structs2.rs
@@ -2,8 +2,6 @@
 // 解决所有的 TODOs 以让测试通过！
 // 执行 `rustlings hint structs2` 或在观察模式下使用 `hint` 子命令来获取提示。
 
-// I AM NOT DONE
-
 #[derive(Debug)]
 struct Order {
     name: String,
@@ -35,7 +33,11 @@ mod tests {
     fn your_order() {
         let order_template = create_order_template();
         // TODO: 使用更新语法和上面的模板来创建你自己的订单！
-        // let your_order =
+        let your_order = Order {
+            name: String::from("Hacker in Rust"),
+            count: 1,
+            ..order_template
+        };
         assert_eq!(your_order.name, "Hacker in Rust");
         assert_eq!(your_order.year, order_template.year);
         assert_eq!(your_order.made_by_phone, order_template.made_by_phone);
