08d1501337dcc6f7830417c4529e8a8a6a2a1b45
focksor <focksor@outlook.com>
2023-01-11 21:37:55 +0800

    solve move_semantics4

--- exercises/move_semantics/move_semantics4.rs
+++ exercises/move_semantics/move_semantics4.rs
@@ -3,12 +3,8 @@
 // 而是在函数中创建 Vector 然后将其传回 main 函数。
 // 执行 `rustlings hint move_semantics4` 或在观察模式下使用 `hint` 子命令来获取提示。
 
-// I AM NOT DONE
-
 fn main() {
-    let vec0 = Vec::new();
-
-    let mut vec1 = fill_vec(vec0);
+    let mut vec1 = fill_vec();
 
     println!("{} has length {} content `{:?}`", "vec1", vec1.len(), vec1);
 
@@ -19,7 +15,7 @@ fn main() {
 
 // `fill_vec()` 不再使用 `vec: Vec<i32>` 作为参数
 fn fill_vec() -> Vec<i32> {
-    let mut vec = vec;
+    let mut vec = Vec::new();
 
     vec.push(22);
     vec.push(44);
