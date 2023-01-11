e2cfa6c69acc69f22272d0cea62a3fcb491421de
focksor <focksor@outlook.com>
2023-01-11 22:51:14 +0800

    solve structs3

--- exercises/structs/structs3.rs
+++ exercises/structs/structs3.rs
@@ -4,8 +4,6 @@
 // 使代码可以编译并通过测试！
 // 执行 `rustlings hint structs3` 或在观察模式下使用 `hint` 子命令来获取提示。
 
-// I AM NOT DONE
-
 #[derive(Debug)]
 struct Package {
     sender_country: String,
@@ -26,12 +24,14 @@ impl Package {
         }
     }
 
-    fn is_international(&self) -> ??? {
+    fn is_international(&self) -> bool {
         // 这边有东西...
+        self.sender_country != self.recipient_country
     }
 
-    fn get_fees(&self, cents_per_gram: i32) -> ??? {
+    fn get_fees(&self, cents_per_gram: i32) -> i32 {
         // 这边有东西...
+        cents_per_gram * self.weight_in_grams
     }
 }
 
