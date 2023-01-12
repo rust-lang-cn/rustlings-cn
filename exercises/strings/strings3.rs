e682bb5ea02c95cd9a2ed4548aeeafda99df6eb1
focksor <focksor@outlook.com>
2023-01-12 23:49:28 +0800

    solve strings3

--- exercises/strings/strings3.rs
+++ exercises/strings/strings3.rs
@@ -1,21 +1,19 @@
 // strings3.rs
 // 执行 `rustlings hint strings3` 或在观察模式下使用 `hint` 子命令来获取提示。
 
-// I AM NOT DONE
-
 fn trim_me(input: &str) -> String {
     // TODO: 删除字符串两端的空格！
-    ???
+    input.trim().to_string()
 }
 
 fn compose_me(input: &str) -> String {
     // TODO: 添加 " world!" 到字符串！有好几种方法可以做到！
-    ???
+    String::from(input) + " world!"
 }
 
 fn replace_me(input: &str) -> String {
     // TODO: 将字符串中的 "cars" 替换为 "balloons"！
-    ???
+    String::from(input).replace("cars", "balloons")
 }
 
 #[cfg(test)]
