77f0293909e725a026c64dbd046a061aaaccce25
focksor <focksor@outlook.com>
2023-01-14 22:50:50 +0800

    solve modules3

--- exercises/modules/modules3.rs
+++ exercises/modules/modules3.rs
@@ -5,10 +5,8 @@
 // 如果你能用一行完成，就可以获得额外的风格积分！
 // 执行 `rustlings hint modules3` 或在观察模式下使用 `hint` 子命令来获取提示。
 
-// I AM NOT DONE
-
 // TODO: 完成这个 use 语句
-use ???
+use std::time::{ SystemTime, UNIX_EPOCH };
 
 fn main() {
     match SystemTime::now().duration_since(UNIX_EPOCH) {
