eee39835d3e443d554eb08fbdd38a0b91c7156c4
focksor <focksor@outlook.com>
2023-01-14 21:30:04 +0800

    solve modules1

--- exercises/modules/modules1.rs
+++ exercises/modules/modules1.rs
@@ -1,15 +1,13 @@
 // modules1.rs
 // 执行 `rustlings hint modules1` 或在观察模式下使用 `hint` 子命令来获取提示。
 
-// I AM NOT DONE
-
 mod sausage_factory {
     // 不要让模块外的任何人看到这个！
     fn get_secret_recipe() -> String {
         String::from("Ginger")
     }
 
-    fn make_sausage() {
+    pub fn make_sausage() {
         get_secret_recipe();
         println!("sausage!");
     }
