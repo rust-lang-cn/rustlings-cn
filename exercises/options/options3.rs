bc577f7490782e3bf65a01aec979652549945f44
focksor <focksor@outlook.com>
2023-01-15 22:31:12 +0800

    solve options3

--- exercises/options/options3.rs
+++ exercises/options/options3.rs
@@ -1,8 +1,6 @@
 // options3.rs
 // 执行 `rustlings hint options3` 或在观察模式下使用 `hint` 子命令来获取提示。
 
-// I AM NOT DONE
-
 struct Point {
     x: i32,
     y: i32,
@@ -12,7 +10,7 @@ fn main() {
     let y: Option<Point> = Some(Point { x: 100, y: 200 });
 
     match y {
-        Some(p) => println!("Co-ordinates are {},{} ", p.x, p.y),
+        Some(ref p) => println!("Co-ordinates are {},{} ", p.x, p.y),
         _ => println!("no match"),
     }
     y; // 修复问题但不能删除这行
