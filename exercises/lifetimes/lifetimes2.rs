05f8d8d7e8312990f9ef60deecdb08fd14428b3d
focksor <focksor@outlook.com>
2023-01-19 23:13:18 +0800

    solve lifetimes2

--- exercises/lifetimes/lifetimes2.rs
+++ exercises/lifetimes/lifetimes2.rs
@@ -4,8 +4,6 @@
 //
 // 执行 `rustlings hint lifetimes2` 或在观察模式下使用 `hint` 子命令来获取提示。
 
-// I AM NOT DONE
-
 fn longest<'a>(x: &'a str, y: &'a str) -> &'a str {
     if x.len() > y.len() {
         x
@@ -20,6 +18,6 @@ fn main() {
     {
         let string2 = String::from("xyz");
         result = longest(string1.as_str(), string2.as_str());
+        println!("The longest string is '{}'", result);
     }
-    println!("The longest string is '{}'", result);
 }
