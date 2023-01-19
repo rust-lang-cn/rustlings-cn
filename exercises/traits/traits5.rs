bdd2c50ee9252825be03bea7e69f8cc3d044eb26
focksor <focksor@outlook.com>
2023-01-19 15:27:11 +0800

    solve traits5

--- exercises/traits/traits5.rs
+++ exercises/traits/traits5.rs
@@ -4,8 +4,6 @@
 // 不要修改此外的任何行。
 // 执行 `rustlings hint traits5` 或在观察模式下使用 `hint` 子命令来获取提示。
 
-// I AM NOT DONE
-
 pub trait SomeTrait {
     fn some_function(&self) -> bool {
         true
@@ -27,7 +25,7 @@ impl SomeTrait for OtherStruct {}
 impl OtherTrait for OtherStruct {}
 
 // 你只能修改下一行
-fn some_func(item: ??) -> bool {
+fn some_func(item: impl SomeTrait + OtherTrait) -> bool {
     item.some_function() && item.other_function()
 }
 
