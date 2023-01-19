6bba3b2866059629c65f5bcd03d22db329384752
focksor <focksor@outlook.com>
2023-01-19 21:43:54 +0800

    solve quiz3

--- exercises/quiz3.rs
+++ exercises/quiz3.rs
@@ -12,15 +12,15 @@
 
 // 执行 `rustlings hint quiz3` 或在观察模式下使用 `hint` 子命令来获取提示。
 
-// I AM NOT DONE
+use std::fmt::Display;
 
-pub struct ReportCard {
-    pub grade: f32,
+pub struct ReportCard<T> {
+    pub grade: T,
     pub student_name: String,
     pub student_age: u8,
 }
 
-impl ReportCard {
+impl<T: Display> ReportCard<T> {
     pub fn print(&self) -> String {
         format!("{} ({}) - achieved a grade of {}",
             &self.student_name, &self.student_age, &self.grade)
@@ -48,7 +48,7 @@ mod tests {
     fn generate_alphabetic_report_card() {
         // TODO: 完成练习后，要修改这个成绩
         let report_card = ReportCard {
-            grade: 2.1,
+            grade: "A+",
             student_name: "Gary Plotter".to_string(),
             student_age: 11,
         };
