4e606c25b757858f077c5fbfbb09c54a00c2b8e0
focksor <focksor@outlook.com>
2023-01-19 13:20:34 +0800

    solve generics1

--- exercises/generics/generics1.rs
+++ exercises/generics/generics1.rs
@@ -3,9 +3,7 @@
 
 // 执行 `rustlings hint generics1` 或在观察模式下使用 `hint` 子命令来获取提示。
 
-// I AM NOT DONE
-
 fn main() {
-    let mut shopping_list: Vec<?> = Vec::new();
+    let mut shopping_list: Vec<&str> = Vec::new();
     shopping_list.push("milk");
 }
