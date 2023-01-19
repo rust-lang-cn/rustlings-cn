111ea467684ce568774eeb0424aef35fc54a049a
focksor <focksor@outlook.com>
2023-01-19 22:13:54 +0800

    solve tests1

--- exercises/tests/tests1.rs
+++ exercises/tests/tests1.rs
@@ -6,12 +6,10 @@
 // 这个测试有一个问题 -- 使测试可以编译！使测试通过！使测试失败！
 // 执行 `rustlings hint tests1` 或在观察模式下使用 `hint` 子命令来获取提示。
 
-// I AM NOT DONE
-
 #[cfg(test)]
 mod tests {
     #[test]
     fn you_can_assert() {
-        assert!();
+        assert!(1==1 && true);
     }
 }
