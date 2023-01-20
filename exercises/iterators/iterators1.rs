51ca59956add13e31be078817efc0a1ed491f776
focksor <focksor@outlook.com>
2023-01-20 13:36:49 +0800

    solve iterators1

--- exercises/iterators/iterators1.rs
+++ exercises/iterators/iterators1.rs
@@ -8,17 +8,15 @@
 //
 // 执行 `rustlings hint iterators1` 或在观察模式下使用 `hint` 子命令来获取提示。
 
-// I AM NOT DONE
-
 fn main () {
     let my_fav_fruits = vec!["banana", "custard apple", "avocado", "peach", "raspberry"];
 
-    let mut my_iterable_fav_fruits = ???;   // TODO: 步骤 1
+    let mut my_iterable_fav_fruits = my_fav_fruits.iter();   // TODO: 步骤 1
 
     assert_eq!(my_iterable_fav_fruits.next(), Some(&"banana"));
-    assert_eq!(my_iterable_fav_fruits.next(), ???);     // TODO: 步骤 2
+    assert_eq!(my_iterable_fav_fruits.next(), Some(&"custard apple"));     // TODO: 步骤 2
     assert_eq!(my_iterable_fav_fruits.next(), Some(&"avocado"));
-    assert_eq!(my_iterable_fav_fruits.next(), ???);     // TODO: 步骤 3
+    assert_eq!(my_iterable_fav_fruits.next(), Some(&"peach"));     // TODO: 步骤 3
     assert_eq!(my_iterable_fav_fruits.next(), Some(&"raspberry"));
-    assert_eq!(my_iterable_fav_fruits.next(), ???);     // TODO: 步骤 4
+    assert_eq!(my_iterable_fav_fruits.next(), None);     // TODO: 步骤 4
 }
