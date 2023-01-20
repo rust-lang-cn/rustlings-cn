67fd518b5d69c2f8ed35cde65abdc8ac9a14ce1c
focksor <focksor@outlook.com>
2023-01-20 15:14:07 +0800

    solve threads1

--- exercises/threads/threads1.rs
+++ exercises/threads/threads1.rs
@@ -2,8 +2,6 @@
 // 执行 `rustlings hint threads1` 或在观察模式下使用 `hint` 子命令来获取提示。
 // 这个程序应等待所有派生线程完成后再退出。
 
-// I AM NOT DONE
-
 use std::thread;
 use std::time::Duration;
 
@@ -12,15 +10,16 @@ fn main() {
 
     let mut handles = vec![];
     for i in 0..10 {
-        thread::spawn(move || {
+        handles.push(thread::spawn(move || {
             thread::sleep(Duration::from_millis(250));
             println!("thread {} is complete", i);
-        });
+        }));
     }
 
     let mut completed_threads = 0;
     for handle in handles {
         // TODO: 从 thread::spawn 返回了一个结构，你可以使用它吗？
+        handle.join();
         completed_threads += 1;
     }
 
