fad8a76446c76001761c60c98fbdafd7c7fb13ad
focksor <focksor@outlook.com>
2023-01-20 17:04:54 +0800

    solve cow1

--- exercises/smart_pointers/cow1.rs
+++ exercises/smart_pointers/cow1.rs
@@ -5,8 +5,6 @@
 // 它可以封装并提供一个借用数据的不可变访问，并当需要修改或所有权时克隆数据。
 // 该类型旨在通过 Borrow trait 处理一般借用数据。
 
-// I AM NOT DONE
-
 use std::borrow::Cow;
 
 fn abs_all<'a, 'b>(input: &'a mut Cow<'b, [i32]>) -> &'a mut Cow<'b, [i32]> {
@@ -42,7 +40,7 @@ fn main() {
     let mut input = Cow::from(slice);
     match abs_all(&mut input) {
         // TODO
-        Cow::Borrowed(_) => println!("I own this slice!"),
+        Cow::Owned(_) => println!("I own this slice!"),
         _ => panic!("expected borrowed value"),
     }
 }
